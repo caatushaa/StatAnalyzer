4cf6ea8e51843b2569e9652bd0aa43ccb759df74
caatushaa <katavesel@gmail.com>
2026-05-12 17:57:54 +0300

    Initial project structure with main form and folder layout

---
+++ StatAnalyzer/Forms/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatAnalyzer.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Text = "Статистический анализатор";
+            this.Size = new Size(400, 300);
+
+            var btnGdp = new Button
+            {
+                Text = "ВВП и ВНП России",
+                Size = new Size(300, 60),
+                Location = new Point(50, 60)
+            };
+            btnGdp.Click += (s, e) => new GdpForm().Show();
+
+            var btnRoads = new Button
+            {
+                Text = "Качество дорог по субъектам",
+                Size = new Size(300, 60),
+                Location = new Point(50, 150)
+            };
+            btnRoads.Click += (s, e) => new RoadsForm().Show();
+
+            Controls.Add(btnGdp);
+            Controls.Add(btnRoads);
+        }
+    }
+}
