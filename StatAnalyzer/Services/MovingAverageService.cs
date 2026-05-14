49a89f978b082e3cd97bd712e8b3f97a17afc1d1
caatushaa <katavesel@gmail.com>
2026-05-14 10:57:46 +0300

    Изменен сервиc скользящей средней

--- StatAnalyzer/Services/MovingAverageService.cs
+++ StatAnalyzer/Services/MovingAverageService.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StatAnalyzer.Services
 {
-    internal class MovingAverageService
+    public class MovingAverageService
     {
+        public List<double> Forecast(List<double> data, int windowSize, int forecastSteps)
+        {
+            var result = new List<double>(data);
+            for (int i = 0; i < forecastSteps; i++)
+            {
+                var window = result.Skip(result.Count - windowSize).Take(windowSize);
+                double avg = window.Average();
+                result.Add(avg);
+            }
+            return result.Skip(data.Count).ToList();
+        }
     }
 }
